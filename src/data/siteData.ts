1f1d86f2831c3a19c5d0c9455894fc9301e1a2e5
Macbook ของ BALL <pannawat.s2904@gmail.com>
2026-09-09 16:41:51 +0700

    feat: update evaluation form Google Drive link to compressed version

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -448,8 +448,8 @@ export const evaluationDocuments = [
   {
     id: "1",
     title: "แบบประเมินผลการฝึกสอน (ครั้งที่ 1)",
-    pdfUrl: "https://drive.google.com/file/d/1NLlwB-t0HwWp09i50mq3ge7hc5t1Z8T4/preview",
-    driveUrl: "https://drive.google.com/file/d/1NLlwB-t0HwWp09i50mq3ge7hc5t1Z8T4/view?usp=sharing",
+    pdfUrl: "https://drive.google.com/file/d/1hh-BvzgC_Rxfg6aL1b26FwQmJ-4fibHW/preview",
+    driveUrl: "https://drive.google.com/file/d/1hh-BvzgC_Rxfg6aL1b26FwQmJ-4fibHW/view?usp=sharing",
   }
 ];
 
